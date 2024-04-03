6575150830238fdfe62dacccb287b594538e7472
ray-yhc <raycho521@yonsei.ac.kr>
2024-04-03 15:37:29 +0900

    [INIT] initial settings

---
+++ src/main/java/com/example/apiexception/ApiExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiExceptionApplication.class, args);
+	}
+
+}
